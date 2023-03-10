e33ec8c4401a2cfac7d1ae70972b94620fa31389
debian <tjdghgud12@naver.com>
2023-03-10 15:56:38 +0900

    2023-03-10

--- server/index.js
+++ server/index.js
@@ -1,19 +1,19 @@
 const express = require('express');
 const { EnkaClient } = require('enka-network-api');
+const genshin = require('genshin-api');
 const enka = new EnkaClient();
 const app = express();
 
 app.get('/', function(req, res){
-    
-});
-
-enka.fetchUser(840110542).then( user => {
-    let character = user.charactersPreview;
-    character.map((d, i) => {
-        console.log(`Character: ${d.characterData._nameId}(Lv: ${d.level})`);
-        console.log('test');
+    enka.fetchUser(840110542).then( user => {
+        let character = user.charactersPreview;
+        // 1. 캐릭터 정보를 본다.
+        // 2. 캐릭터 정보에 따라서 genshin 이미지를 불러온다.
+        character.map((d, i) => {
+            console.log(`Character: ${d.characterData._nameId}(Lv: ${d.level})`);
+        })
     })
-})
+});
 
 // const test = enka.getAllCharacters();
 // console.log(test.map(d => d.name.get("en")));
