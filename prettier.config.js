15aa29a9df479282bbbd9eab2c43d700977043e5
성호형 <tjdghgud12@naver.com>
2025-07-14 17:05:49 +0900

    feat: eslint 및 prettier 적용

---
+++ prettier.config.js
@@ -0,0 +1,10 @@
+/** @type {import("prettier").Config} */
+
+const prettierConfig = {
+  endOfLine: "crlf", // CRLF로 줄바꿈 강제
+  semi: true,
+  trailingComma: "all",
+  printWidth: 180,
+};
+
+export default prettierConfig;
