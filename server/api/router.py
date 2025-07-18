8930850c4fe084bd140df8ae34046103be998a34
성호형 <tjdghgud12@naver.com>
2025-07-18 14:48:52 +0900

    fix : fast api 코드 정리

--- server/api/router.py
+++ server/api/router.py
@@ -1,4 +1,3 @@
-
 from fastapi import APIRouter
 import ambr
 from services.parseEnka import parseFightProps
@@ -13,14 +12,14 @@ ENKA_API_URL = "https://enka.network/api/uid/{uid}"  # uid는 유저 ID
 
 @router.get("/user/{uid}")
 async def getUserData(uid: int):
-    async with enka.GenshinClient(lang='ko') as client:
+    async with enka.GenshinClient(lang="ko") as client:
         rawRes = await client.fetch_showcase(uid, raw=True)
         avatar_info_list = client.parse_showcase(rawRes).characters
 
         # 필요한 정보만 추출 예시
         parsed_avatars = []
         for i, avatar in enumerate(avatar_info_list):
-            avatarRawData = rawRes['avatarInfoList'][i]
+            avatarRawData = rawRes["avatarInfoList"][i]
             name = avatar.name
             artifacts = avatar.artifacts
             weapon = avatar.weapon
@@ -28,24 +27,27 @@ async def getUserData(uid: int):
             characterInfo = {
                 "name": name,
                 "level": avatar.level,
-                "icon": avatar.costume.icon if getattr(avatar, "costume", None) else avatar.icon,
-                "baseStat":{**characterStats, **{key: value for key, value in fightProps.items() if "BASE" in key}},
+                "icon": avatar.costume.icon if getattr(avatar, "costume", None) else avatar.icon,  # type: ignore
+                "baseStat": {**characterStats, **{key: value for key, value in fightProps.items() if "BASE" in key}},
                 "weapon": {},
                 "artifact": {},
                 "activeSkill": {},
                 "passiveSkill": {},
-                "totalStat":{},
+                "totalStat": {},
             }
 
             # ----------------------- 캐릭터 Base Stat 제작 -----------------------
             # 캐릭터 레벨에 따른 추가 stat 까지 적용
             async with ambr.AmbrAPI(lang=ambr.Language.KR) as ambrApi:
-                ambrCharacterDetail = await ambrApi.fetch_character_detail(avatar.id)
+                ambrCharacterDetail = await ambrApi.fetch_character_detail(str(avatar.id))
             promoteStat = max(
-                (promote for promote in ambrCharacterDetail.upgrade.promotes 
-                if promote.unlock_max_level <= int(characterInfo["level"])),
+                (
+                    promote
+                    for promote in ambrCharacterDetail.upgrade.promotes
+                    if promote.unlock_max_level <= int(characterInfo["level"])
+                ),
                 key=lambda x: x.unlock_max_level,
-                default=0
+                default=0,
             )
             if promoteStat:
                 for addStat in promoteStat.add_stats:
@@ -63,14 +65,16 @@ async def getUserData(uid: int):
             # 1. 무기 옵션은 일단 오늘은 불가능. 무기별로 전부 함수 별도로 만들어야 하기 때문에
             # 2. 주옵 부옵만 적용하기
             # 무기에 담아야 하는 값: name, refinement(재련), stat
-            characterInfo['weapon']['name'] = weapon.name
-            characterInfo['weapon']['refinement'] = weapon.refinement
-            characterInfo['weapon']['level'] = weapon.level
-            characterInfo['weapon']['icon'] = weapon.icon
-            characterInfo['weapon']['option'] = weaponInfo.weaponType[weapon.name] if getattr(weaponInfo.weaponType, weapon.name, None) else []
-            characterInfo['weapon']['stat'] = {}
+            characterInfo["weapon"]["name"] = weapon.name
+            characterInfo["weapon"]["refinement"] = weapon.refinement
+            characterInfo["weapon"]["level"] = weapon.level
+            characterInfo["weapon"]["icon"] = weapon.icon
+            characterInfo["weapon"]["option"] = (
+                weaponInfo.weaponType[weapon.name] if getattr(weaponInfo.weaponType, weapon.name, None) else []
+            )
+            characterInfo["weapon"]["stat"] = {}
             for stat in weapon.stats:
-                characterInfo['weapon']['stat'][stat.type.value] = stat.value
+                characterInfo["weapon"]["stat"][stat.type.value] = stat.value
 
             # 이제 각 무기별 option에 대한 리턴값은 별도로 보관하는게 좋겠다.
             # 1차적으로 무기 옵션의 type 지정 완료
@@ -89,9 +93,9 @@ async def getUserData(uid: int):
             # 패시브의 경우 조건없이 스텟 증가할 경우 적용. 아닌 경우는 front에서 on/off 적용
             # ---------------------------------------------------------------------
 
-            
             parsed_avatars.append(characterInfo)
         return {"avatars": parsed_avatars}
+
 
 @router.get("/")
 async def read_root():
--- server/main.py
+++ server/main.py
@@ -1,11 +1,7 @@
 from fastapi import FastAPI
 from api import router
 
-
-test = "dasdsa"
-
 app = FastAPI()
-
 
 
 app.include_router(router, prefix="/api")
