0c8ad66c2d295311458316b55b7512e3fc396a06
성호형 <tjdghgud12@naver.com>
2025-07-25 17:20:44 +0900

    feat: 무기 성장 곡선 추가

--- server/services/ambrApi.py
+++ server/services/ambrApi.py
@@ -7,22 +7,26 @@ ambrApi = None
 async def initAmbrApi():
     global ambrApi
     from data import character
+    from data import weapon
 
     if ambrApi is None:
         ambrApi = ambr.AmbrAPI(lang=Language.KR)
         await ambrApi.start()
         character.ambrCharacterCurve = await ambrApi.fetch_avatar_curve()
+        weapon.ambrWeaponCurve = await ambrApi.fetch_weapon_curve()
     return ambrApi
 
 
 async def closeAmbrApi():
     global ambrApi
     from data import character
+    from data import weapon
 
     if ambrApi is not None:
         await ambrApi.close()
         ambrApi = None
-        character.ambrCharacterCurve = None
+        character.ambrCharacterCurve = {}
+        weapon.ambrWeaponCurve = {}
 
 
 async def getAmbrApi():
