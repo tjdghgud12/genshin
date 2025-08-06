20434a1ad1b346fcee78f7eaab5183fe84048c21
성호형 <tjdghgud12@naver.com>
2025-08-06 15:01:56 +0900

    feat: nextjs 기초 생성 완료

---
+++ client/eslint.config.mjs
@@ -0,0 +1,50 @@
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+import { FlatCompat } from "@eslint/eslintrc";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+  resolvePluginsRelativeTo: __dirname,
+});
+
+const eslintConfig = [
+  ...compat.extends("next/core-web-vitals", "next/typescript", "plugin:@typescript-eslint/recommended", "plugin:react/recommended", "plugin:prettier/recommended"),
+  {
+    ignores: ["eslint.config.js", "eslint.config.mjs", "node_modules/", ".next/", "dist/", "prettier.config.js", "postcss.config.cjs"],
+    languageOptions: {
+      parser: (await import("@typescript-eslint/parser")).default,
+      parserOptions: {
+        ecmaFeatures: { jsx: true },
+        ecmaVersion: 2021,
+        sourceType: "module",
+        tsconfigRootDir: __dirname,
+        project: "./tsconfig.json",
+      },
+    },
+    plugins: {
+      "@typescript-eslint": (await import("@typescript-eslint/eslint-plugin")).default,
+      react: (await import("eslint-plugin-react")).default,
+      prettier: (await import("eslint-plugin-prettier")).default,
+    },
+    rules: {
+      "no-unused-vars": "off",
+      "@typescript-eslint/no-unused-vars": ["warn", { argsIgnorePattern: "^_" }],
+      "react/prop-types": "off",
+      "react/jsx-uses-react": "off",
+      "react/react-in-jsx-scope": "off",
+      "prettier/prettier": "error",
+      "@typescript-eslint/explicit-function-return-type": "warn",
+      "linebreak-style": ["error", "windows"],
+    },
+    settings: {
+      react: {
+        version: "detect",
+      },
+    },
+  }
+];
+
+export default eslintConfig;
---
+++ client/next.config.ts
@@ -0,0 +1,12 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  turbopack:{},
+  // webpack(config, { isServer }) {
+  //   // 필요시 다음과 같이 설정하여 PnP 문제를 해결
+  //   config.resolve.symlinks = false;
+  //   return config;
+  // },
+};
+
+export default nextConfig;
