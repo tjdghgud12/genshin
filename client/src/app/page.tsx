f501b70fced50768f75e30ec8956e31c1fdcef5d
성호형 <tjdghgud12@naver.com>
2025-08-08 08:39:32 +0900

    feat: client-루트 페이지 uid input 및 캐릭터 데이터 조회 추가

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,104 +1,90 @@
-import Image from "next/image";
+"use client";
 
-export default function Home() {
+import { Button } from "@/components/ui/button";
+import { Form, FormControl, FormDescription, FormField, FormItem, FormMessage } from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import api from "@/lib/axios";
+import { useCalculatorStore } from "@/store/useCalculatorStore";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useRouter } from "next/navigation";
+import React from "react";
+import { useForm } from "react-hook-form";
+import { toast, Toaster } from "sonner";
+import { z } from "zod";
+
+// **************************** Schema ****************************
+const uidFormSchema = z.object({
+  uid: z.string().min(9, { error: "UID를 확인해주세요." }).max(12, {
+    error: "UID를 확인해주세요.",
+  }),
+});
+// **************************** Schema ****************************
+
+const Home = (): React.ReactElement => {
+  const router = useRouter();
+  const setCalculatorData = useCalculatorStore((state) => state.setTotalCalculatorData);
+
+  const form = useForm<z.infer<typeof uidFormSchema>>({
+    resolver: zodResolver(uidFormSchema),
+    defaultValues: {
+      uid: "",
+    },
+  });
+
+  const handleUid = (e: React.ChangeEvent<HTMLInputElement>): void => {
+    const uid = e.target.value;
+    if (/^\d*$/.test(uid)) {
+      form.setValue("uid", uid);
+    }
+  };
+
+  const onSubmit = (valus: z.infer<typeof uidFormSchema>): void => {
+    toast.promise(api.get(`/api/user/${valus.uid}`), {
+      loading: "로딩 중",
+      success: (res) => {
+        setCalculatorData(res.data.characters);
+        const searchParams = new URLSearchParams({ uid: valus.uid });
+        router.push(`/calculator?${searchParams.toString()}`);
+        return "성공요";
+      },
+      error: (err) => {
+        console.log(err);
+        return "실패요";
+      },
+    });
+  };
 
   return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="font-mono list-inside list-decimal text-sm/6 text-center sm:text-left">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] font-mono font-semibold px-1 py-0.5 rounded">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+    <div>
+      <main className="w-screen h-screen">
+        <div className="w-screen h-full flex flex-col">
+          <Toaster richColors />
+          {/* 해당 위치에 뭔가 꾸밀만한 것을더 넣업자 */}
+          <div />
+          <Form {...form}>
+            <form onSubmit={form.handleSubmit(onSubmit)} className="w-full min-w-[1100px] flex mx-auto">
+              <FormField
+                control={form.control}
+                name="uid"
+                render={({ field }) => (
+                  <FormItem id="qawdasd" className="w-1/7 h-fit mr-1 ml-auto">
+                    <FormControl>
+                      <Input placeholder="UID" className=" p-0 text-center" {...field} onChange={handleUid} maxLength={12} />
+                    </FormControl>
+                    <FormDescription />
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <Button type="submit" className="mr-auto">
+                Submit
+              </Button>
+            </form>
+          </Form>
         </div>
       </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
     </div>
   );
-}
+};
+
+export default Home;
