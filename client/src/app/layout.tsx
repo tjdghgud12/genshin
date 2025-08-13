3acfb89a9f51ee5b81f36737da5aff2a6188f635
성호형 <tjdghgud12@naver.com>
2025-08-13 15:38:28 +0900

    fix: client-최외각 width, height 변경(screen -> full)

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -10,7 +10,7 @@ export const metadata: Metadata = {
 const RootLayout = ({ children }: Readonly<{ children: React.ReactNode }>): React.ReactElement => {
   return (
     <html lang="en">
-      <body className="w-screen h-screen min-w-[1200px] min-h-[500px]">{children}</body>
+      <body className="w-full h-full min-w-[1200px] min-h-[500px]">{children}</body>
     </html>
   );
 };
