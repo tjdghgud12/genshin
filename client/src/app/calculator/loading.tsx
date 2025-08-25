943d9d8c98e51fa4ef5f070f943d705955202c99
성호형 <tjdghgud12@naver.com>
2025-08-25 13:52:25 +0900

    feat: client-calculator page loading 화면 추가

---
+++ client/src/app/calculator/loading.tsx
@@ -0,0 +1,16 @@
+import { Dot } from "lucide-react";
+import React from "react";
+
+const CalculatorPageLoading = (): React.ReactElement => {
+  return (
+    <div className="w-screen h-screen flex">
+      <div className="flex items-center m-auto">
+        <Dot className="w-26 h-26 text-violet-600 animate-bounce stroke-7 bounce-with-pause" style={{}} />
+        <Dot className="w-26 h-26 text-violet-600 animate-bounce stroke-7 bounce-with-pause" style={{ animationDelay: "0.5s" }} />
+        <Dot className="w-26 h-26 text-violet-600 animate-bounce stroke-7 bounce-with-pause" style={{ animationDelay: "1s" }} />
+      </div>
+    </div>
+  );
+};
+
+export default CalculatorPageLoading;
