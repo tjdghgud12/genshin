7266bf77fa5f88a30bdb555a77e6514f1651dd6f
성호형 <tjdghgud12@naver.com>
2025-08-26 16:49:32 +0900

    feat: client-객체 merge 함수 추가

--- client/src/lib/utils.ts
+++ client/src/lib/utils.ts
@@ -1,6 +1,8 @@
 import { clsx, type ClassValue } from "clsx";
 import { twMerge } from "tailwind-merge";
 
+type AnyObject = Record<string, any>;
+
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
@@ -23,4 +25,34 @@ const inputNumberWithSpace = (val: string, useFloat?: boolean, decimalPlaces?: n
   return useFloat ? Number(cleaned) : Number.parseInt(cleaned);
 };
 
-export { inputNumberWithSpace };
+const deepMergeAddOnly = <T extends AnyObject, S extends AnyObject>(target: T, source: S): T & S => {
+  const result: AnyObject = { ...target };
+
+  for (const key in source) {
+    const sourceVal = source[key];
+    const targetVal = target[key];
+
+    if (targetVal === undefined) {
+      result[key] = sourceVal;
+    } else if (sourceVal && typeof sourceVal === "object" && targetVal && typeof targetVal === "object") {
+      if (Array.isArray(sourceVal) && Array.isArray(targetVal)) {
+        const minLength = Math.min(sourceVal.length, targetVal.length);
+        result[key] = targetVal.map((item: (typeof targetVal)[number], idx: number) => {
+          if (idx < minLength) {
+            if (item && typeof item === "object" && sourceVal[idx] && typeof sourceVal[idx] === "object") {
+              return deepMergeAddOnly(item as AnyObject, sourceVal[idx]);
+            } else return item;
+          } else return item;
+        });
+      } else if (!Array.isArray(sourceVal) && !Array.isArray(targetVal)) {
+        result[key] = deepMergeAddOnly(targetVal, sourceVal);
+      } else result[key] = targetVal;
+    } else {
+      result[key] = targetVal;
+    }
+  }
+
+  return result as T & S;
+};
+
+export { deepMergeAddOnly, inputNumberWithSpace };
