00079ff7d6a5be74bc744000c85a989ccff0ac1d
성호형 <tjdghgud12@naver.com>
2025-09-02 14:42:47 +0900

    fix: client-run 상태에 따라 api 주소 변경되도록 적용

--- client/src/lib/axios.ts
+++ client/src/lib/axios.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_SHH_CALCULATOR_API_URL,
+  baseURL: process.env.NODE_ENV === "development" ? process.env.NEXT_PUBLIC_GENSHIN_SETTING_CALCULATOR_API_DEV : process.env.NEXT_PUBLIC_GENSHIN_SETTING_CALCULATOR_API,
 });
 
 export default api;
