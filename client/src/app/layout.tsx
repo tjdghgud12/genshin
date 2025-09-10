de0dde5cc6a2703791adda0c00916966395d6bf5
성호형 <tjdghgud12@naver.com>
2025-09-10 16:57:23 +0900

    fix: client-favicon위치 조정

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -1,8 +1,19 @@
 import Store from "@/app/Store";
 import api from "@/lib/axios";
 import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
 import React from "react";
 import "./globals.css";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
 
 export const metadata: Metadata = {
   title: "Genshin Impact Setting Calculator",
@@ -15,7 +26,7 @@ const RootLayout = async ({ children }: Readonly<{ children: React.ReactNode }>)
 
   return (
     <html className="h-full" lang="en">
-      <body className="min-h-screen grid grid-rows-[1fr_auto]">
+      <body className={`${geistSans.variable} ${geistMono.variable} antialiased min-h-screen grid grid-rows-[1fr_auto]`}>
         <Store weaponList={weaponList} artifactSets={artifactSets}>
           <div className="flex flex-col justify-center">{children}</div>
         </Store>
