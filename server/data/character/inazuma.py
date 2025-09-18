66e35f7376a00e5628fbbdc1e42f244f011d31f0
성호형 <tjdghgud12@naver.com>
2025-09-18 16:32:59 +0900

    feat-server-요이미야 추가(캐릭터)

--- server/data/character/inazuma.py
+++ server/data/character/inazuma.py
@@ -127,4 +127,69 @@ info = {
             ),
         ],
     ),
+    "요이미야": characterDataSchema(
+        passiveSkill={
+            "소매불 백경도": passiveSkillSchema(
+                description="염초 정화(庭火)의 춤 지속 시간 동안 요이미야의 일반 공격이 명중되면 요이미야에게 불 원소 피해 보너스를 2% 제공",
+                unlockLevel=1,
+                options=[skillConstellationOptionSchema(type=skillConstellationType.stack, maxStack=10, label="일반 공격 명중")],
+            ),
+            "한낮의 풍물시": passiveSkillSchema(
+                description="유금 운간초를 발동하면 고유 특성 「소매불 백경도」의 중첩 스택에 따라 1스택마다 추가로 공격력이 1% 증가",
+                unlockLevel=4,
+                options=[skillConstellationOptionSchema(type=skillConstellationType.toggle, maxStack=1, label="")],
+            ),
+        },
+        activeSkill={
+            "쏘아 올린 불꽃": activeSkillSchema(
+                baseFightProp=skillBaseFightPropSchema(
+                    nomal=damageBaseFightPropSchema(ATTACK=1, element=["physical"]),
+                    charge=damageBaseFightPropSchema(ATTACK=1, element=["physical", "fire"]),
+                    falling=damageBaseFightPropSchema(ATTACK=1, element=["physical", "fire"]),
+                ),
+                additionalAttack=[additionalAttackSchema(name="염초 화살(강공격)", type="fire", baseFightProp=damageBaseFightPropSchema(ATTACK=1, element=["fire"]))],
+            ),
+            "염초 정화(庭火)의 춤": activeSkillSchema(
+                description="일반 공격이 염초 화살로 전환되며, 일반공격 피해가 최종 연산으로 증가",
+                options=[skillConstellationOptionSchema(type=skillConstellationType.toggle, maxStack=1, label="")],
+            ),
+            "유금 운간초": activeSkillSchema(
+                baseFightProp=skillBaseFightPropSchema(elementalBurst=damageBaseFightPropSchema(ATTACK=1, element=["fire"])),
+                additionalAttack=[additionalAttackSchema(name="유금화광", type="fire", baseFightProp=damageBaseFightPropSchema(ATTACK=1, element=["fire"]))],
+            ),
+        },
+        constellation=[
+            contellationSchema(
+                name="적옥의 유금",
+                description="유금 운간초의 유금화광 지속 시간이 4초 증가. 요이미야의 유금화광 상태의 적이 효과 지속 시간 내에 처치되면, 요이미야의 공격력이 20% 증가",
+                options=[skillConstellationOptionSchema(type=skillConstellationType.toggle, maxStack=1, label="")],
+            ),
+            contellationSchema(
+                name="만등 점화",
+                description="요이미야의 불 원소 피해가 치명타를 입힌 후, 요이미야는 6초 동안 25%의 불 원소 피해 보너스를 획득. 요이미야가 대기 상태일 때도 해당 효과를 획득",
+                options=[skillConstellationOptionSchema(type=skillConstellationType.toggle, maxStack=1, label="")],
+            ),
+            contellationSchema(
+                name="쥐불놀이",
+                description="원소 전투 스킬 레벨 +3",
+                options=[skillConstellationOptionSchema(type=skillConstellationType.always, maxStack=1, label="")],
+            ),
+            contellationSchema(
+                name="폭죽장인의 깨달음",
+                description="요이미야 자신의 유금화광이 폭발할 때 염초 정화(庭火)의 춤의 재사용 대기시간이 1.2초 감소한다.",
+                options=[skillConstellationOptionSchema(type=skillConstellationType.toggle, maxStack=1, label="")],
+            ),
+            contellationSchema(
+                name="한여름의 전야제",
+                description="원소 폭발 레벨 +3",
+                options=[skillConstellationOptionSchema(type=skillConstellationType.always, maxStack=1, label="")],
+            ),
+            contellationSchema(
+                name="나가노하라 유성군",
+                description="염초 정화(庭火)의 춤 지속 시간 동안 요이미야가 일반 공격을 사용하면, 50%의 확률로 염초 화살을 추가로 1발 더 발사해 기존 피해의 60%에 해당하는 피해를 준다. 해당 피해는 일반 공격 피해로 간주한다.",
+                options=[skillConstellationOptionSchema(type=skillConstellationType.always, maxStack=1, label="")],
+                additionalAttack=[additionalAttackSchema(name="염초 화살(6돌)", type="nomal", baseFightProp=damageBaseFightPropSchema(ATTACK=0.6, element=["fire"]))],
+            ),
+        ],
+    ),
 }
--- server/services/character/inazuma.py
+++ server/services/character/inazuma.py
@@ -69,10 +69,10 @@ async def getRaidenShogunFightProp(ambrCharacterDetail: CharacterDetail, charact
                 case "강철 절단":
                     if constellation.options[0].active:
                         newFightProp.add(fightPropMpa.DEFENSE_IGNORE.value, 0.6)
-                # case "진영의 과거":
-                #     characterInfo.activeSkill[2].level -= 3 if enkaDataFlag else 0
-                # case "쇼군의 현형":
-                #     characterInfo.activeSkill[1].level -= 3 if enkaDataFlag else 0
+                case "진영의 과거":
+                    characterInfo.activeSkill[2].level -= 3 if enkaDataFlag else 0
+                case "쇼군의 현형":
+                    characterInfo.activeSkill[1].level -= 3 if enkaDataFlag else 0
 
     # ----------------------- active -----------------------
     activeSkillLevelMap = {
@@ -122,7 +122,85 @@ async def getRaidenShogunFightProp(ambrCharacterDetail: CharacterDetail, charact
     return CharacterFightPropReturnData(fightProp=newFightProp, characterInfo=characterInfo)
 
 
+async def getYoimiyaFightProp(ambrCharacterDetail: CharacterDetail, characterInfo: requestCharacterInfoSchema, enkaDataFlag: bool = False) -> CharacterFightPropReturnData:
+    newFightProp: fightPropSchema = genCharacterBaseStat(ambrCharacterDetail, int(characterInfo.level))
+
+    # -----------------------weapon & Artifact -----------------------
+    weaponArtifactData = await getWeaponArtifactFightProp(deepcopy(newFightProp), characterInfo.weapon, characterInfo.artifact)
+    newFightProp = weaponArtifactData["fightProp"]
+
+    # ----------------------- constellations -----------------------
+    # 폭죽장인의 깨달음, 나가노하라 유성군의 경우 fightProp에 영행 X
+    for constellation in characterInfo.constellations:
+        if constellation.unlocked:
+            match constellation.name:
+                case "적옥의 유금":
+                    if constellation.options[0].active:
+                        newFightProp.add(fightPropMpa.ATTACK_PERCENT.value, 0.2)
+                case "만등 점화":
+                    if constellation.options[0].active:
+                        newFightProp.add(fightPropMpa.FIRE_ADD_HURT.value, 0.25)
+                case "쥐불놀이":
+                    characterInfo.activeSkill[2].level -= 3 if enkaDataFlag else 0
+                case "한여름의 전야제":
+                    characterInfo.activeSkill[1].level -= 3 if enkaDataFlag else 0
+
+    # ----------------------- active -----------------------
+    activeSkillLevelMap = {
+        "염초 정화(庭火)의 춤": [
+            0.379,  # Level 1
+            0.402,  # Level 2
+            0.425,  # Level 3
+            0.454,  # Level 4
+            0.477,  # Level 5
+            0.499,  # Level 6
+            0.529,  # Level 7
+            0.558,  # Level 8
+            0.588,  # Level 9
+            0.617,  # Level 10
+            0.647,  # Level 11
+            0.677,  # Level 12
+            0.706,  # Level 13
+            0.736,  # Level 14
+            0.765,  # Level 15
+        ]
+    }
+    # 염초 정화(庭火)의 춤
+    for active in characterInfo.activeSkill:
+        match active.name:
+            case "염초 정화(庭火)의 춤":
+                if active.options[0].active:
+                    addElementalSkillLevel = next((constellation for constellation in characterInfo.constellations if constellation.name == "쥐불놀이"))
+                    addLevel = 3 if addElementalSkillLevel.unlocked else 0
+                    skillValue = activeSkillLevelMap[active.name][active.level + addLevel - 1]
+                    if characterInfo.activeSkill[0].baseFightProp.nomal is not None and characterInfo.activeSkill[0].baseFightProp.nomal.element is not None:
+                        characterInfo.activeSkill[0].baseFightProp.nomal.element.append("fire")
+                    newFightProp.add(fightPropMpa.FINAL_NOMAL_ATTACK_ATTACK_ADD_HURT.value, skillValue)
+
+    # ----------------------- passive -----------------------
+    for passive in characterInfo.passiveSkill:
+        if passive.unlocked:
+            match passive.name:
+                case "소매불 백경도":
+                    if passive.options[0].active:
+                        newFightProp.add(fightPropMpa.FIRE_ADD_HURT.value, passive.options[0].stack * 0.02)
+
+                case "한낮의 풍물시":
+                    if passive.options[0].active:
+                        firstPassive = next((passive for passive in characterInfo.passiveSkill if passive.name == "소매불 백경도"))
+                        if firstPassive.options[0].active:
+                            newFightProp.add(fightPropMpa.ATTACK_PERCENT.value, firstPassive.options[0].stack * 0.01)
+
+    # ----------------------- 추후 연산 진행부 -----------------------
+    newFightProp = await getAfterWeaponArtifactFightProp(
+        weaponArtifactData["fightProp"], characterInfo.weapon, characterInfo.artifact, weaponArtifactData["weaponAfterProps"], weaponArtifactData["artifactAfterProps"]
+    )
+
+    return CharacterFightPropReturnData(fightProp=newFightProp, characterInfo=characterInfo)
+
+
 getFightProp: dict[str, CharacterFightPropGetter] = {
     "카미사토 아야카": getKamisatoAyakaFightProp,
     "라이덴 쇼군": getRaidenShogunFightProp,
+    "요이미야": getYoimiyaFightProp,
 }
