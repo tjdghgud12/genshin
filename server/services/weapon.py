0ba59ad4ef22ddf2de29fca10916a0259d6bee5a
성호형 <tjdghgud12@naver.com>
2025-10-28 14:44:47 +0900

    feat: server-페보니우스 검, 바위산을 맴도는 노래 추가(무기)

--- server/services/weapon.py
+++ server/services/weapon.py
@@ -813,6 +813,76 @@ async def getFavoniusLanceFightProp(
     return {"fightProp": fightProp, "afterAddProps": None}
 
 
+async def getFavoniusSwordFightProp(
+    id: int, level: int, _refinement: int, _options: list[weaponDataSchema.extendedWeaponOptionSchema], _characterFightProp: fightPropSchema
+) -> WeaponDataReturnSchema:
+    fightProp = await getWeaponBaseFightProp(id, level)
+
+    return {"fightProp": fightProp, "afterAddProps": None}
+
+
+async def getPeakPatrolSongFightProp(
+    id: int, level: int, refinement: int, options: list[weaponDataSchema.extendedWeaponOptionSchema], characterFightProp: fightPropSchema
+) -> WeaponDataReturnSchema:
+    fightProp = await getWeaponBaseFightProp(id, level)
+    optionRefinementMap = [
+        [0.08, 0.10, 0.08, 0.256],
+        [0.10, 0.125, 0.10, 0.32],
+        [0.12, 0.15, 0.12, 0.384],
+        [0.14, 0.175, 0.14, 0.448],
+        [0.16, 0.20, 0.16, 0.512],
+    ]
+    refinementValue = optionRefinementMap[refinement - 1]
+
+    for i, option in enumerate(options):
+        if option.active:
+            match i:
+                case 0:
+                    # 1. 방어력 8/10/12/14/16% 증가
+                    fightProp.add(fightPropMpa.DEFENSE_PERCENT.value, refinementValue[0] * option.stack)
+                    # 2. 모든 원소 피해 보너스 10/12.5/15/17.5/20% 증가
+                    value = refinementValue[1] * option.stack
+                    fightProp.add(fightPropMpa.FIRE_ADD_HURT.value, value)
+                    fightProp.add(fightPropMpa.WATER_ADD_HURT.value, value)
+                    fightProp.add(fightPropMpa.ELEC_ADD_HURT.value, value)
+                    fightProp.add(fightPropMpa.WIND_ADD_HURT.value, value)
+                    fightProp.add(fightPropMpa.GRASS_ADD_HURT.value, value)
+                    fightProp.add(fightPropMpa.ICE_ADD_HURT.value, value)
+                    fightProp.add(fightPropMpa.ROCK_ADD_HURT.value, value)
+                    # 3. 해당 효과 2스택 중첩 또는 2스택의 지속 시간 갱신 시, 장착 캐릭터의 방어력에 기반해 1000pt마다 파티 내 주변에 있는 모든 캐릭터의 모든 원소 피해 보너스가 8/10/12/14/16% 증가하고,
+                    # 최대 25.6/32/38.4/44.8/51.2%까지 증가한다.
+                    if option.stack == option.maxStack:
+                        baseDefense = characterFightProp.FIGHT_PROP_BASE_DEFENSE
+                        characterDefense = characterFightProp.FIGHT_PROP_DEFENSE
+                        characterDefensePercent = characterFightProp.FIGHT_PROP_DEFENSE_PERCENT
+                        characterDefenseTotal = baseDefense * (characterDefensePercent + 1)
+                        characterDefenseTotal = characterDefenseTotal + characterDefense
+                        additionalElementHurt = int(characterDefenseTotal / 1000) * refinementValue[2]
+
+                        if additionalElementHurt > refinementValue[3]:
+                            additionalElementHurt = refinementValue[3]
+                        fightProp.add(fightPropMpa.FIRE_ADD_HURT.value, additionalElementHurt)
+                        fightProp.add(fightPropMpa.WATER_ADD_HURT.value, additionalElementHurt)
+                        fightProp.add(fightPropMpa.ELEC_ADD_HURT.value, additionalElementHurt)
+                        fightProp.add(fightPropMpa.WIND_ADD_HURT.value, additionalElementHurt)
+                        fightProp.add(fightPropMpa.GRASS_ADD_HURT.value, additionalElementHurt)
+                        fightProp.add(fightPropMpa.ICE_ADD_HURT.value, additionalElementHurt)
+                        fightProp.add(fightPropMpa.ROCK_ADD_HURT.value, additionalElementHurt)
+
+    return {
+        "fightProp": fightProp,
+        "afterAddProps": [
+            fightPropMpa.FIRE_ADD_HURT.value,
+            fightPropMpa.WATER_ADD_HURT.value,
+            fightPropMpa.ELEC_ADD_HURT.value,
+            fightPropMpa.WIND_ADD_HURT.value,
+            fightPropMpa.GRASS_ADD_HURT.value,
+            fightPropMpa.ICE_ADD_HURT.value,
+            fightPropMpa.ROCK_ADD_HURT.value,
+        ],
+    }
+
+
 getTotalWeaponFightProp = {
     "아모스의 활": getAmosBowFightProp,
     "안개를 가르는 회광": getMistsplitterReforgedFightProp,
@@ -849,4 +919,6 @@ getTotalWeaponFightProp = {
     "결투의 창": getDeathmatchFightProp,
     "약수": getAquaSimulacraFightProp,
     "페보니우스 장창": getFavoniusLanceFightProp,
+    "페보니우스 검": getFavoniusSwordFightProp,
+    "바위산을 맴도는 노래": getPeakPatrolSongFightProp,
 }
