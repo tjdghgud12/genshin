1e9345a0007d6e25d43700808f5d858381e0e144
성호형 <tjdghgud12@naver.com>
2025-11-11 11:30:51 +0900

    refactor: Store 컴포넌트 제거 및 server단에서 zustand데이터 set 진행

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -1,10 +1,12 @@
-import Store from "@/app/Store";
 import api from "@/lib/axios";
+import { artifactSetsInfoStore } from "@/store/artifact/artifactStore";
+import { weaponInfoStore } from "@/store/weapon/weaponStore";
 import { IArtifactSetsInfo } from "@/types/artifactType";
 import { IWeaponInfo } from "@/types/weaponType";
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import React from "react";
+import { Toaster } from "sonner";
 import "./globals.css";
 
 const geistSans = Geist({
@@ -25,13 +27,16 @@ export const metadata: Metadata = {
 const RootLayout = async ({ children }: Readonly<{ children: React.ReactNode }>): Promise<React.ReactElement> => {
   const weaponList = Object.fromEntries((await api.get(`/weapons`)).data.map((weapon: IWeaponInfo) => [weapon.id, weapon]));
   const artifactSets = Object.fromEntries((await api.get(`/artifactsets`)).data.map((set: IArtifactSetsInfo) => [set.name, set]));
+  weaponInfoStore.setState({ weaponList });
+  artifactSetsInfoStore.setState({ artifactSets });
 
   return (
     <html className="h-full" lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased min-h-screen grid grid-rows-[1fr_auto]`}>
-        <Store weaponList={weaponList} artifactSets={artifactSets}>
-          <div className="flex flex-col justify-center">{children}</div>
-        </Store>
+        <div>
+          <Toaster richColors />
+          {children}
+        </div>
         <footer className="text-gray-600 p-2 mt-5 mx-auto">Created by JjoriButler</footer>
       </body>
     </html>
