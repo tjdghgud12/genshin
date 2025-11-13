3782113dd56b406427cc0a40eec0d891ae4a3e75
성호형 <tjdghgud12@naver.com>
2025-11-13 13:53:40 +0900

    fix: client-Type을 type폴더에 선언 후 사용하도록 수정 및 공용 type에 맞춰 데이터 수정

--- client/src/lib/parseCharacterInfo.ts
+++ client/src/lib/parseCharacterInfo.ts
@@ -1,57 +1,37 @@
 import { calculatorCharacterInfoSchema } from "@/lib/calculator";
+import { IUidSearchResult } from "@/types/calculatorType";
 import { z } from "zod";
 import { fightPropLabels } from "./fightProps";
 
-type TCalculatorData = z.infer<typeof calculatorCharacterInfoSchema>;
+export type TCalculatorData = z.infer<typeof calculatorCharacterInfoSchema>;
 
-interface IuserSelectoptions {
-  id: number;
-  level?: number;
-  numberOfParts?: number;
-  unlocked?: boolean;
-  options: {
-    level?: number;
-    unlocked?: boolean;
-    stack: number;
-    active: boolean;
-    select?: string | null;
-    maxStack?: number;
-    type: string;
-    selectList: (string | null)[];
-    description: string;
-    label: string;
-    requiredParts: number;
-  }[];
-  [key: string]: unknown;
-}
-
-const parseCharacterInfo = <T extends { characterInfo: Record<string, any> }>(rawCalculatorData: T): TCalculatorData => {
+const parseCharacterInfo = (rawCalculatorData: IUidSearchResult): TCalculatorData => {
   const data = rawCalculatorData.characterInfo;
   return {
-    raw: data,
     level: data.level,
-    constellations: data.constellations.map((constellation: IuserSelectoptions) => ({
-      unlocked: constellation.unlocked,
+    constellations: data.constellations.map((constellation) => ({
+      unlocked: constellation.unlocked ?? false,
       options: constellation.options.map((o) => ({
         stack: o.stack,
         active: o.active,
       })),
     })),
-    activeSkill: data.activeSkill.map((skill: IuserSelectoptions) => ({
+    activeSkill: data.activeSkill.map((skill) => ({
       level: (skill.level as number) > 10 ? 10 : skill.level, // 돌파에 따른 스킬렙 증가는 별도로 back에서 연산하기 위해 최대값을 10으로 제한
       options: skill.options.map((option) => ({ active: option.active, stack: option.stack })),
     })),
-    passiveSkill: data.passiveSkill.map((skill: IuserSelectoptions) => ({
+    passiveSkill: data.passiveSkill.map((skill) => ({
       unlocked: skill.unlocked,
       options: skill.options.map((option) => ({ active: option.active, stack: option.stack })),
     })),
+
     weapon: {
       id: data.weapon.id,
       name: data.weapon.name,
       type: data.weapon.type,
       level: data.weapon.level,
       refinement: data.weapon.refinement,
-      options: data.weapon.options.map((o: IuserSelectoptions) => ({
+      options: data.weapon.options.map((o) => ({
         active: o.active,
         stack: o.stack,
         select: o.select,
@@ -63,32 +43,28 @@ const parseCharacterInfo = <T extends { characterInfo: Record<string, any> }>(ra
       })),
     },
     artifact: {
-      parts: data.artifact.parts.map((part: { setName: string; type: string; mainStat: { [key: string]: number }; subStat: { [key: string]: number }[] }) => ({
+      parts: data.artifact.parts.map((part) => ({
         setName: part.setName,
         type: part.type,
         mainStat: Object.fromEntries(Object.entries(part.mainStat).map(([k, v]) => [k, fightPropLabels[k].includes("%") ? (v * 100).toFixed(2) : v])),
-        subStat: part.subStat.map((s: { [key: string]: number }) =>
-          Object.fromEntries(Object.entries(s).map(([k, v]) => [k, fightPropLabels[k].includes("%") ? (v * 100).toFixed(2) : v])),
-        ),
+        subStat: part.subStat.map((s) => Object.fromEntries(Object.entries(s).map(([k, v]) => [k, fightPropLabels[k].includes("%") ? (v * 100).toFixed(2) : v]))),
       })),
-      setInfo: data.artifact.setInfo.map((s: IuserSelectoptions) => ({
+      setInfo: data.artifact.setInfo.map((s) => ({
         id: s.id,
         name: s.name,
         numberOfParts: s.numberOfParts,
-        options: s.options
-          ? s.options.map((o) => {
-              return {
-                requiredParts: o.requiredParts,
-                active: o.active,
-                stack: o.stack,
-                maxStack: o.maxStack,
-                type: o.type,
-                selectList: o.selectList,
-                description: o.description,
-                label: o.label,
-              };
-            })
-          : [],
+        options: s.options.map((o) => {
+          return {
+            requiredParts: o.requiredParts,
+            active: o.active,
+            stack: o.stack,
+            maxStack: o.maxStack,
+            type: o.type,
+            selectList: o.selectList,
+            description: o.description,
+            label: o.label,
+          };
+        }),
       })),
     },
   };
