79ea4ec8396c3072536b9d01c5d774b7f06b708f
hohyeong12.seong <hohyeong12.seong@lgepartner.com>
2025-12-08 15:14:54 +0900

    style: client loading.tsx 영역을 screen -> full 로변경

--- client/src/app/loading.tsx
+++ client/src/app/loading.tsx
@@ -3,7 +3,7 @@ import React from "react";
 
 const RootLoading = (): React.ReactElement => {
   return (
-    <div className="w-screen h-screen flex">
+    <div className="w-full h-full flex">
       <div className="flex items-center m-auto">
         <Dot className="w-26 h-26 text-violet-600 animate-bounce stroke-7 bounce-with-pause" style={{}} />
         <Dot className="w-26 h-26 text-violet-600 animate-bounce stroke-7 bounce-with-pause" style={{ animationDelay: "0.5s" }} />
